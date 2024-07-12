5d362f3fb6cca77f2124fb679890b2aecc3b9b8c
iSoulEs <83812194+iSoulEs@users.noreply.github.com>
2024-07-12 03:49:14 -0300

    balance 50% de daño ejecutar a 3%

--- src/main/java/yesman/epicfight/gameasset/Skills.java
+++ src/main/java/yesman/epicfight/gameasset/Skills.java
@@ -221,7 +221,7 @@ public class Skills {
 				.addProperty(AttackPhaseProperty.STUN_TYPE, StunType.HOLD)
 				.newPropertyLine()
 				.addProperty(AttackPhaseProperty.MAX_STRIKES, ValueCorrector.setter(1))
-				.addProperty(AttackPhaseProperty.EXTRA_DAMAGE, ExtraDamageType.get(ExtraDamageType.PERCENT_OF_TARGET_LOST_HEALTH, 0.5F))
+				.addProperty(AttackPhaseProperty.EXTRA_DAMAGE, ExtraDamageType.get(ExtraDamageType.PERCENT_OF_TARGET_LOST_HEALTH, 0.03F))
 				.addProperty(AttackPhaseProperty.ARMOR_NEGATION, ValueCorrector.adder(50.0F))
 				.addProperty(AttackPhaseProperty.STUN_TYPE, StunType.LONG)
 				.registerPropertiesToAnimation());
